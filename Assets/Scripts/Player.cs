215eff123d1060cbc4a6cf1278df332b7d299392
Jetinat0r <jetin8t0r@gmail.com>
2025-02-22 19:37:26 -0500

    Added Color and Hands to Players

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,10 +9,25 @@ public class Player : MonoBehaviour
     public string userName = "Player";
     public int color = 0;
 
+    [SerializeField]
+    public SpriteRenderer baseColor;
+    [SerializeField]
+    public Transform pivot;
+    [SerializeField]
+    public Transform handHolder;
+    [SerializeField]
+    public SpriteRenderer[] hands = new SpriteRenderer[2];
+
     public void SetSpawnInfo(ushort _id, string _userName, int _color)
     {
         id = _id;
         userName = _userName;
         color = _color;
+
+        baseColor.color = GameManager.instance.playerColors[color];
+        foreach(SpriteRenderer _hand in hands)
+        {
+            _hand.color = GameManager.instance.playerColors[color];
+        }
     }
 }
